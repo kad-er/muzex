691c5c1476eb25d1c768b179762a507f38a4563e
Salim Hachemaoui <shachemaoui544@gmail.com>
2022-05-27 03:05:30 +0100

    mise a jour de projet

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,12 @@ const tabTriggers = document.getElementsByClassName("tab-trigger");
 const menuTabs = document.getElementById("menu-tabs");
 const mainSlider = document.getElementById("main-slider");
 const header = document.getElementById("header");
+const mutebtn = document.getElementById('muteBtn')
 
+
+//the media
+const audio = document.getElementById('audio')
+var muted = false;
 function watchNavBar() {
   // get the header height first
 }
@@ -16,7 +21,21 @@ function ui() {
     tt.addEventListener("click", () => {
       document.getElementById("offcanvas-img").classList.add("d-none");
     });
+    
   }
- 
+  mutebtn.addEventListener('click',()=>{
+    // alert(muted)
+    console.log(audio)
+    audio.muted = !muted;
+    muted = !muted;
+    console.log("this is his fucking child",mutebtn.firstChild)
+    if(muted){
+      mutebtn.firstElementChild.classList.replace("fa-volume-high","fa-volume-off")
+    }else{
+      mutebtn.firstElementChild.classList.replace("fa-volume-off","fa-volume-high")
+
+    }
+  })
+  AOS.init();
   //the nav bar scroll
 }
